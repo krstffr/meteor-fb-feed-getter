9f224ac138068d80981875fc292ad033529bef9c
Kristoffer Klintberg <krstffr@gmail.com>
2014-10-30 10:53:04 +0100

    Updated to new package structure.

--- package.js
+++ package.js
@@ -1,12 +1,15 @@
 Package.describe({
-  "summary": "Gets the news feed from Facebook Pages. (This could do much more with some extra work...)"
+  summary: "Gets the news feed from Facebook Pages. (This could do much more in the future as well...)",
+  name: "krstffr:fb-feed-getter",
+  version: "1.0.2",
+  git: "https://github.com/krstffr/meteor-fb-feed-getter"
 });
 
 Package.onUse(function (api) {
 
-  api.use('underscore', 'server');
+  api.use('underscore', 'HTTP', 'server');
 
-  api.add_files('lib/facebook-feed-getter.js', 'server');
+  api.add_files('fb-feed-getter.js', 'server');
 
 	api.export('FacebookFeedGetter', 'server');
 
